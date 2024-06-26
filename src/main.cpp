b343278ef2bc6717352e009ff047834734fe49d3
Keftos <76626274+Thomas-xX@users.noreply.github.com>
2024-06-26 16:33:23 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -35,11 +35,11 @@ char auth[] = "0aecdf657f20";
 #define wifichoose 1 // 选择使用场景  0：家 1：公司
 
 #if (wifichoose == 0)
-#define SSID "QQQQ"
+#define SSID "QQQQ"//替换成自己的wifi名称和密码
 #define PSWD "000000000"
 
 #elif (wifichoose == 1)
-#define SSID "qqqq"
+#define SSID "qqqq"//替换成自己的wifi名称和密码
 #define PSWD "11111111"
 
 #endif
